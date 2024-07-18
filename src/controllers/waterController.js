72904f180544c3cd10158dd25f8781b8495268b2
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-07-18 11:25:21 +0300

    delete No records Found

--- src/controllers/waterController.js
+++ src/controllers/waterController.js
@@ -47,16 +47,16 @@ const getDailyWaterVolumeController = async (req, res) => {
 };
  
 
-const getMonthlyWaterVolumeController = async (req, res,next) => {
+const getMonthlyWaterVolumeController = async (req, res) => {
         const { month, year } = req.params;
         const monthInt = parseInt(month);
         const yearInt = parseInt(year);
 
         const volumeRecords = await Services.water.getMonthlyWaterVolume(req.user._id, { month: monthInt, year: yearInt });
 
-        if (!volumeRecords || volumeRecords.length === 0) {
-            return next(HttpError(404, 'No records found for the specified month and year'));
-        }
+        // if (!volumeRecords || volumeRecords.length === 0) {
+        //     return next(HttpError(404, 'No records found for the specified month and year'));
+        // }
 
         const dailyVolumes = {};
         volumeRecords.forEach(record => {
